b0d2f40de9e3f6855f0377f65e13816e32c354c5
chrysmur <sostomc011@gmail.com>
2026-03-06 02:34:38 +0300

    remove redundant types

--- src/PortMan.Api/Authentication/Types/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PortMan.Api.Authentication.Types;
-
-public class LoginRequest
-{
-    public string TenantId { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
--- src/PortMan.Api/Authentication/Types/TokenResponse.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace PortMan.Api.Authentication.Types;
-
-public record TokenResponse(string AccessToken, string TokenType, int ExpiresIn);
